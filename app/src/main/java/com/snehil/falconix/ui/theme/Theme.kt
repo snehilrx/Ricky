06cf92b047c091e98084ce048aca4fda1c1b9276
snehil <101540623+hashsalt0@users.noreply.github.com>
2024-07-27 00:50:03 +0530

    Changed theme name

--- app/src/main/java/com/snehil/falconix/ui/theme/Theme.kt
+++ app/src/main/java/com/snehil/falconix/ui/theme/Theme.kt
@@ -252,7 +252,7 @@ val unspecified_scheme = ColorFamily(
 )
 
 @Composable
-fun AppTheme(
+fun FalconIXTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
